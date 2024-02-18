93bddf592a7012b85f7af4db2de868c78ec4cb64
Rosalea Anderson <156455009+smugCrick3t@users.noreply.github.com>
2024-02-18 12:31:11 -0700

    added SCC finder functions

--- generate_output.cpp
+++ generate_output.cpp
@@ -1,13 +1,90 @@
-#include <fstream>
 #include <iostream>
+#include <vector>
+#include <deque>
+#include <unordered_set>
+#include <fstream>
 #include <stack>
 #include <string>
-#include <unordered_set>
-#include <vector>
+
 using namespace std;
 
+void sortVisit(vector<unordered_set<int>>& g, int u, vector<char>& color, deque<int>& toposort) {
+    color[u] = 'g';
+    for (int v : g[u]) {
+        if (color[v] == 'w') {
+            sortVisit(g, v, color, toposort);
+        }
+    }
+    color[u] = 'b';
+    // when a vertex has been fully processed it can be added to the front of the toposort list
+    // this forces the last vertex to be processed to be the first vertex popped from toposort
+    toposort.push_front(u);
+}
 
+deque<int> sortGraph(vector<unordered_set<int>>& g) {
+    // perform DFS to toposort the graph
+    deque<int> toposort;
+    vector<char> color(g.size(), 'w');
+    int numVertices = g.size();
+    for (int u = 0; u < numVertices; ++u) {
+        if (color[u] == 'w') {
+            sortVisit(g, u, color, toposort);
+        }
+    }
+    return toposort;
+}
 
+vector<unordered_set<int>> makeGTranspose(vector<unordered_set<int>>& g) {
+    vector<unordered_set<int>> newGraph(g.size());
+    for (size_t i = 0; i < g.size(); ++i) {
+        for (int vertex : g[i]) {
+            newGraph[vertex].insert(i);
+        }
+    }
+    return newGraph;
+}
+
+void sccVisit(int u, vector<unordered_set<int>>& gTranspose, unordered_set<int>& scc, vector<char>& color) {
+    color[u] = 'g';
+    for (int v : gTranspose[u]) {
+        if (color[v] == 'w') {
+            scc.insert(v);
+            sccVisit(v, gTranspose, scc, color);
+        }
+    }
+    color[u] = 'b';
+}
+
+deque<unordered_set<int>> generateSccForest(deque<int>& topoSort, vector<unordered_set<int>>& gTranspose) {
+    deque<unordered_set<int>> sccForest;
+    vector<char> color(gTranspose.size(), 'w');
+    while (!topoSort.empty()) {
+        int u = topoSort.front();
+        topoSort.pop_front();
+        // perform DFS on gTranspose in the order of toposorted vertices
+        if (color[u] == 'w') {
+            unordered_set<int> scc;
+            scc.insert(u);
+            sccVisit(u, gTranspose, scc, color);
+            if (scc.size() > 1) {
+                sccForest.push_back(scc);
+            }
+        }
+    }
+    return sccForest;
+}
+
+// this is just a debugging function
+void printSccForest(deque<unordered_set<int>> sccForest){
+    for (int i = 0; i < sccForest.size(); i++) {
+        unordered_set<int> scc = sccForest[i];
+        cout << "SCC:";
+        for (const auto& vertex : scc){
+            cout << " " << vertex;
+        }
+        cout << endl;
+    }
+}
 
 int main(int argc, char* argv[]){
 
@@ -23,12 +100,13 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    // adjacency lists
-    vector<unordered_set<int>> graph, graphReverse;
-    
     // read input
     int numVertices;
     inputFile >> numVertices;
+    
+    // adjacency lists
+    vector<unordered_set<int>> graph(numVertices+1), graphReverse(numVertices+1);
+    
     for (int i = 1; i < numVertices+1; i++){
         int numEdges, vertex;
         inputFile >> numEdges;
@@ -41,8 +119,13 @@ int main(int argc, char* argv[]){
     }
     inputFile.close();
 
-
-    // TODO: turn graph into strongly connected components
+    // toposort the original graph
+    deque<int> topoSort = sortGraph(graph);
+    // I wrote a function to generate the transpose for when we test graphs after running the algorithm
+    // but for the first scc generation we can use the reverse graph generated above 
+    vector<unordered_set<int>> gTranspose = makeGTranspose(graph);
+    // generate sccForest from the toposort and gTranspose
+    deque<unordered_set<int>> sccForest = generateSccForest(topoSort, gTranspose);
 
 
     // TODO: run algorithm
