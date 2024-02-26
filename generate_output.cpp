058aa07fbeef40af393b4cb66278e72666355155
aidanccraft <aidan.c.craft@gmail.com>
2024-02-25 19:29:38 -0700

    Fixed bugs in generate and verify outputs

--- generate_output.cpp
+++ generate_output.cpp
@@ -6,6 +6,7 @@
 #include <stack>
 #include <string>
 #include <unordered_map>
+#include <chrono>
 
 using namespace std;
 
@@ -25,16 +26,22 @@ void sortVisit(adjacency_list& g, int u, unordered_map<int, char>& color, deque<
 }
 
 deque<int> sortGraph(adjacency_list& g) {
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+
     // perform DFS to toposort the graph
     deque<int> toposort;
     unordered_map<int, char> color;
     for (const auto& u : g) color[u.first] = 'w';
-    int numVertices = g.size();
+
     for (const auto& u : g) {
         if (color[u.first] == 'w') {
             sortVisit(g, u.first, color, toposort);
         }
     }
+
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "sortGraph " << (chrono::duration_cast<chrono::microseconds>(end - begin).count()) / 1000000.0 << "[s]" << endl;
+
     return toposort;
 }
 
@@ -48,21 +55,20 @@ adjacency_list makeGTranspose(adjacency_list& g) {
     return newGraph;
 }
 
-void sccVisit(int u, adjacency_list& gTranspose, adjacency_list& scc, unordered_map<int, char>& color) {
+void sccVisit(int u, adjacency_list& gTranspose, unordered_set<int>& scc, unordered_map<int, char>& color) {
     color[u] = 'g';
     for (int v : gTranspose[u]) {
         if (color[v] == 'w') {
-            scc[v] = unordered_set<int>();
-            scc[u].insert(v);
+            scc.insert(v);
             sccVisit(v, gTranspose, scc, color);
-        } else if (color[v] == 'g') {
-            scc[u].insert(v);
         }
     }
     color[u] = 'b';
 }
 
-deque<adjacency_list> generateSccForest(deque<int>& topoSort, adjacency_list& gTranspose) {
+deque<adjacency_list> generateSccForest(deque<int>& topoSort, adjacency_list& graph, adjacency_list& gTranspose) {
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+
     deque<adjacency_list> sccForest;
     unordered_map<int, char> color;
     for (const auto& u : gTranspose) color[u.first] = 'w';
@@ -71,37 +77,49 @@ deque<adjacency_list> generateSccForest(deque<int>& topoSort, adjacency_list& gT
         topoSort.pop_front();
         // perform DFS on gTranspose in the order of toposorted vertices
         if (color[u] == 'w') {
+            unordered_set<int> sccNodes = { u };
+            sccVisit(u, gTranspose, sccNodes, color);
             adjacency_list scc;
-            scc[u] = unordered_set<int>();
-            sccVisit(u, gTranspose, scc, color);
+
+            for (int node : sccNodes) {
+                scc[node] = unordered_set<int>();
+                for (int edge : graph[node]) {
+                    if (sccNodes.count(edge)) scc[node].insert(edge);
+                }
+            }
+
             if (scc.size() > 1) {
                 sccForest.push_back(scc);
             }
         }
     }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "generateSccForest " << (chrono::duration_cast<chrono::microseconds>(end - begin).count()) / 1000000.0 << "[s]" << endl;
+
     return sccForest;
 }
 
 // this is just a debugging function
-void printSccForest(deque<adjacency_list> sccForest){
+void printSccForest(deque<adjacency_list> sccForest) {
     for (int i = 0; i < sccForest.size(); i++) {
         adjacency_list& scc = sccForest[i];
         cout << "SCC:";
-        for (const auto& vertex : scc){
+        for (const auto& vertex : scc) {
             cout << " " << vertex.first;
         }
         cout << endl;
     }
 }
 
-void heuristicVisit(int u, adjacency_list& scc, adjacency_list& sccReverse, unordered_map<int, char>& color, 
+void heuristicVisit(int u, adjacency_list& scc, adjacency_list& sccReverse, unordered_map<int, char>& color,
     unordered_map<int, int>& weight, pair<int, int>& highestWeight) {
     color[u] = 'g';
     for (int v : scc[u]) {
         if (scc.count(v)) {
             if (color[v] == 'w') {
                 weight[v] = weight[u] + sccReverse[v].size() - scc[u].size();
-                if (weight[v] > highestWeight.second) highestWeight = make_pair(v, weight[v]);
+                if (weight[v] > highestWeight.second)
+                    highestWeight = make_pair(v, weight[v]);
                 heuristicVisit(v, scc, sccReverse, color, weight, highestWeight);
             }
         }
@@ -115,7 +133,7 @@ int generateHeuristic(adjacency_list& scc, adjacency_list& sccReverse) {
     int start;
 
     // initialize DFS variables
-    for(const auto& node : scc) {
+    for (const auto& node : scc) {
         color[node.first] = 'w';
         weight[node.first] = 0;
         start = node.first;
@@ -127,7 +145,7 @@ int generateHeuristic(adjacency_list& scc, adjacency_list& sccReverse) {
 
     // run DFS on SCC and assign weight to each node
     heuristicVisit(start, scc, sccReverse, color, weight, highestWeight);
-    
+
     // return highest weighted node
     return highestWeight.first;
 }
@@ -136,14 +154,11 @@ deque<adjacency_list> removeNode(adjacency_list& scc, adjacency_list& sccReverse
     // remove node and update graphs
     unordered_set<int> outVertices = scc[removedNode];
     unordered_set<int> inVertices = sccReverse[removedNode];
-    
-    scc[removedNode] = unordered_set<int>();
-    sccReverse[removedNode] = unordered_set<int>();
 
     for (int outVertex : outVertices) {
         sccReverse[outVertex].erase(removedNode);
     }
-    
+
     for (int inVertex : inVertices) {
         scc[inVertex].erase(removedNode);
     }
@@ -153,28 +168,28 @@ deque<adjacency_list> removeNode(adjacency_list& scc, adjacency_list& sccReverse
 
     // calculate sccForest from new graph
     deque<int> topoSort = sortGraph(scc);
-    deque<adjacency_list> sccForest = generateSccForest(topoSort, sccReverse);
+    deque<adjacency_list> sccForest = generateSccForest(topoSort, scc, sccReverse);
 
     return sccForest;
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
     // read command line inputs and open files
-    if (argc != 3){
+    if (argc != 3) {
         cerr << "Usage: " << argv[0] << " [INPUT_FILE] [OUTPUT_FILE]" << endl;
         return 1;
     }
 
     string inputFileName = argv[1];
     ifstream inputFile(inputFileName);
-    if (!inputFile.is_open()){
+    if (!inputFile.is_open()) {
         cerr << "Couldn't open file '" << inputFileName << "'" << endl;
         return 1;
     }
 
     string outputFileName = argv[2];
     ofstream outputFile(outputFileName);
-    if (!outputFile.is_open()){
+    if (!outputFile.is_open()) {
         cerr << "Couldn't open file '" << outputFileName << "'" << endl;
         return 1;
     }
@@ -182,14 +197,19 @@ int main(int argc, char* argv[]){
     // read input
     int numVertices;
     inputFile >> numVertices;
-    
+
     // adjacency lists
     adjacency_list graph, graphReverse;
-    
-    for (int i = 1; i < numVertices+1; i++){
+
+    for (int i = 1; i < numVertices + 1; i++) {
+        graph[i] = unordered_set<int>();
+        graphReverse[i] = unordered_set<int>();
+    }
+
+    for (int i = 1; i < numVertices + 1; i++) {
         int numEdges, vertex;
         inputFile >> numEdges;
-        for (int j = 0; j < numEdges; j++){
+        for (int j = 0; j < numEdges; j++) {
             inputFile >> vertex;
             graphReverse[vertex].insert(i);
             graph[i].insert(vertex);
@@ -199,15 +219,15 @@ int main(int argc, char* argv[]){
 
     // toposort the original graph
     deque<int> topoSort = sortGraph(graph);
-    
+
     // generate sccForest
-    deque<adjacency_list> sccForest = generateSccForest(topoSort, graphReverse);
+    deque<adjacency_list> sccForest = generateSccForest(topoSort, graph, graphReverse);
 
     vector<int> removedNodes;
-    
+
     // run algorithm
     while (!sccForest.empty()) {
-        adjacency_list& scc = sccForest.front();
+        adjacency_list scc = sccForest.front();
         adjacency_list sccReverse = makeGTranspose(scc);
 
         // get node to remove
--- verify_output.cpp
+++ verify_output.cpp
@@ -6,50 +6,53 @@
 #include <vector>
 using namespace std;
 
-bool DFS(int curr, vector<vector<int>> &adj, unordered_set<int> discovered, unordered_set<int> processed){
-    if (processed.count(curr)) return true;
-    if (discovered.count(curr)) return false;
-    discovered.insert(curr);
+bool DFS(int curr, vector<vector<int>>& adj, vector<char>& color) {
+    if (color[curr] == 'b') return true;
+    if (color[curr] == 'g') return false;
 
-    for (int child : adj[curr]){
-        if (!DFS(child, adj, discovered, processed)) return false;
+    color[curr] = 'g';
+
+    for (int child : adj[curr]) {
+        if (!DFS(child, adj, color)) return false;
     }
-    discovered.erase(curr);
-    processed.insert(curr);
+
+    color[curr] = 'b';
     return true;
 }
 
 // check if a graph is a DAG (in O(V+E) time)
-bool isDAG(vector<vector<int>> adj){
-    unordered_set<int> discovered, processed;
+bool isDAG(vector<vector<int>>& adj) {
+    vector<char> color(adj.size(), 'w');
 
     // start DFS from every node
-    for (int start = 1; start < adj.size(); start++){
-        DFS(start, adj, discovered, processed);
+    for (int start = 1; start < adj.size(); start++) {
+        if (!DFS(start, adj, color)) return false;
     }
+
     return true;
 }
 
 
-int main(int argc, char* argv[]){
-
+int main(int argc, char* argv[]) {
     // read command line inputs
-    if (argc != 3){
+    if (argc != 3) {
         cerr << "Usage: " << argv[0] << " [INPUT_FILE] [OUTPUT_FILE]" << endl;
         return 1;
     }
+
     string inputFileName = argv[1];
     string outputFileName = argv[2];
 
     // open input file
     ifstream inputFile(inputFileName);
-    if (!inputFile.is_open()){
+    if (!inputFile.is_open()) {
         cerr << "Couldn't open file '" << inputFileName << "'" << endl;
         return 1;
     }
+
     // open output file
     ifstream outputFile(outputFileName);
-    if (!outputFile.is_open()){
+    if (!outputFile.is_open()) {
         cerr << "Couldn't open file '" << outputFileName << "'" << endl;
         return 1;
     }
@@ -58,7 +61,7 @@ int main(int argc, char* argv[]){
     int numVerticesToRemove, vertex;
     outputFile >> numVerticesToRemove;
     unordered_set<int> removedVertices;
-    for (int i = 0; i < numVerticesToRemove; i++){
+    for (int i = 0; i < numVerticesToRemove; i++) {
         outputFile >> vertex;
         removedVertices.insert(vertex);
     }
@@ -67,21 +70,21 @@ int main(int argc, char* argv[]){
     // create graph (ignoring removed vertices)
     int numVertices;
     inputFile >> numVertices;
-    vector<vector<int>> adj(numVertices+1, vector<int>());
-    for (int i = 1; i < numVertices+1; i++){
+    vector<vector<int>> adj(numVertices + 1, vector<int>());
+    for (int i = 1; i < numVertices + 1; i++) {
         int numEdges, vertex;
         inputFile >> numEdges;
-        for (int j = 0; j < numEdges; j++){
+        for (int j = 0; j < numEdges; j++) {
             inputFile >> vertex;
             // skip if vertex has been removed
             if (removedVertices.count(i) || removedVertices.count(vertex)) continue;
-            adj[vertex].push_back(i);
+            adj[i].push_back(vertex);
         }
     }
     inputFile.close();
 
     // check if graph is a DAG
-    if (isDAG(adj)){
+    if (isDAG(adj)) {
         cout << "success!" << endl;
     } else {
         cout << "failed" << endl;
