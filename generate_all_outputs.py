81e9038f1d66f70473be767b21e6595b4f4ae610
Jayden Pahukula <jayden.pahukula@gmail.com>
2024-02-26 17:42:24 -0700

    add script to generate all outputs

---
+++ generate_all_outputs.py
@@ -0,0 +1,32 @@
+import os
+from subprocess import run
+
+
+INPUT_DIR = "./test/"
+OUTPUT_DIR = "./testOut/"
+
+
+if not os.path.exists(INPUT_DIR):
+    print("Error: input directory does not exist")
+    exit()
+
+if not os.path.exists(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+# loop through every file in input dir
+for file in os.listdir(INPUT_DIR):
+
+    inPath = os.path.join(INPUT_DIR, file)
+    outPath = os.path.join(OUTPUT_DIR, file)
+
+    # do not overwrite existing solutions
+    if os.path.exists(outPath):
+        print(f"output '{outPath}' already exists, skipping...")
+        continue
+
+    # only interested in files that end with .txt
+    if os.path.isdir(inPath): continue
+    if len(inPath) < 5 or inPath[-4:] != ".txt": continue
+
+    # execute
+    run(f"./generate_output {inPath} {outPath}".split())
